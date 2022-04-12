915fe6ad2451df5048e3a9f6bf471f1d150809a3
Sachin dabas <77283739+sachin-dabas@users.noreply.github.com>
2022-04-12 01:41:28 -0400

    my first 2D drawing

--- Test_code/sketch.js
+++ Test_code/sketch.js
@@ -1,10 +1,18 @@
+//@sachin-dabas
+// to setup the canvas
 function setup() 
 {
   createCanvas(800, 800);
 }
 
+//to draw on the canvas
 function draw() 
 {
-  background(290,0,300);
-  rect(0,100,100,100);
+  background(100,10,300);
+  //attributes
+  rectMode(CENTER);
+  //primitive shapes
+  rect(400,400,100,100);
+  line(200,200,500,500);
+  rect(200,200,100,100);
 }
