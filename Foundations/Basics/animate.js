ce6e58bfa75258c39fee5bbef05f45961c3bd177
Sachin dabas <77283739+sachin-dabas@users.noreply.github.com>
2022-04-16 14:04:15 -0400

    update circle animation

--- Foundations/Basics/animate.js
+++ Foundations/Basics/animate.js
@@ -4,18 +4,21 @@
 let circleX = 100;
 let circleR = 20;
 
+//setup the scene
 function setup()
 {
     createCanvas(800,800);
     background(255,0,0);
 }
 
+//interactions with the mouse
 function mousePressed()
 {
     circleX = 0;
     circleR = 0;
 }
 
+//draw it over the canvas
 function draw()
 {
     background(255,0,0);
